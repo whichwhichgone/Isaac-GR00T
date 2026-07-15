f8040e5dd9fd5545cda1f488f840da6a7cfab8ea
whichwhichgone <zhaowei@westlake.edu.cn>
2026-07-15 21:01:37 +0800

    support for multiple subdatasets.

--- examples/GR00T-WholeBodyControl/finetune_g1.sh
+++ examples/GR00T-WholeBodyControl/finetune_g1.sh
@@ -1,20 +1,25 @@
 
 set -x -e
 
-export NUM_GPUS=8
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export NUM_GPUS=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_API_KEY=83793606f810aa3d385ea5d12dbd352514ac54e1
+
+DATASET_PATHS=(
+    "/liujinxin/zhaowei/G1_MOTION/data/G1_real_6D_window_cont_rel_hand_sft"
+    "/liujinxin/zhaowei/G1_MOTION/data/G1_real_6D_window_cont_rel_hand_sft_p2"
+)
 
 torchrun --nproc_per_node=$NUM_GPUS --master_port=29500 \
     gr00t/experiment/launch_finetune.py \
-    --base_model_path models/GR00T-N1.6-3B \
-    --dataset_path /liujinxin/zhaowei/G1_MOTION/data/G1_real_6D_window_cont_rel_hand_all \
+    --base_model_path /wangdonglin2/datasets/checkpoint-30000 \
+    --dataset_path "${DATASET_PATHS[@]}" \
     --embodiment_tag UNITREE_G1_29DOF \
     --num_gpus $NUM_GPUS \
-    --output_dir logs_output/g1_29dof_finetune_real_6D_window_cont_rel_hand_all \
+    --output_dir logs_output/g1_29dof_finetune_real_6D_window_cont_rel_hand_sft_0713 \
     --save_total_limit 3 \
-    --max_steps 40000 \
-    --save_steps 10000 \
+    --max_steps 15000 \
+    --save_steps 3000 \
     --warmup_ratio 0.05 \
     --weight_decay 1e-5 \
     --learning_rate 1e-4 \
--- examples/GR00T-WholeBodyControl/run_gr00t_server.sh
+++ examples/GR00T-WholeBodyControl/run_gr00t_server.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 uv run python gr00t/eval/run_gr00t_server.py \
     --embodiment_tag UNITREE_G1_29DOF \
-    --model_path logs_output/g1_29dof_finetune/checkpoint-4000 \
+    --model_path /wangdonglin2/datasets/checkpoint-30000 \
     --device cuda:0 \
     --host 0.0.0.0 \
     --port 9002
